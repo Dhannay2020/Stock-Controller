4841b2c1cc07120ac826d00b40a04b5bd0f9238f
DMHSoftUKLtd <61027383+Dhannay2020@users.noreply.github.com>
2022-07-07 16:25:34 +0100

    Added Change Password
    Change password form added

---
+++ StockControllerV20/StockControllerV20/FrmChangePassword.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace StockControllerV20
+{
+    partial class FrmChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnChange = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtCurrentPassword = new System.Windows.Forms.TextBox();
+            this.TxtNewPassword = new System.Windows.Forms.TextBox();
+            this.TxtReTypePassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnChange
+            // 
+            this.BtnChange.Location = new System.Drawing.Point(241, 87);
+            this.BtnChange.Name = "BtnChange";
+            this.BtnChange.Size = new System.Drawing.Size(73, 34);
+            this.BtnChange.TabIndex = 0;
+            this.BtnChange.Text = "Change Password";
+            this.BtnChange.UseVisualStyleBackColor = true;
+            this.BtnChange.Click += new System.EventHandler(this.BtnChange_Click);
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Location = new System.Drawing.Point(320, 88);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(56, 33);
+            this.BtnCancel.TabIndex = 1;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            this.BtnCancel.Click += new System.EventHandler(this.BtnCancel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Current Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "New Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Re-Type New Password:";
+            // 
+            // TxtCurrentPassword
+            // 
+            this.TxtCurrentPassword.Location = new System.Drawing.Point(145, 9);
+            this.TxtCurrentPassword.Name = "TxtCurrentPassword";
+            this.TxtCurrentPassword.PasswordChar = '*';
+            this.TxtCurrentPassword.Size = new System.Drawing.Size(231, 20);
+            this.TxtCurrentPassword.TabIndex = 5;
+            // 
+            // TxtNewPassword
+            // 
+            this.TxtNewPassword.Location = new System.Drawing.Point(145, 35);
+            this.TxtNewPassword.Name = "TxtNewPassword";
+            this.TxtNewPassword.PasswordChar = '*';
+            this.TxtNewPassword.Size = new System.Drawing.Size(231, 20);
+            this.TxtNewPassword.TabIndex = 6;
+            // 
+            // TxtReTypePassword
+            // 
+            this.TxtReTypePassword.Location = new System.Drawing.Point(145, 61);
+            this.TxtReTypePassword.Name = "TxtReTypePassword";
+            this.TxtReTypePassword.PasswordChar = '*';
+            this.TxtReTypePassword.Size = new System.Drawing.Size(231, 20);
+            this.TxtReTypePassword.TabIndex = 7;
+            // 
+            // FrmChangePassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 136);
+            this.Controls.Add(this.TxtReTypePassword);
+            this.Controls.Add(this.TxtNewPassword);
+            this.Controls.Add(this.TxtCurrentPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnChange);
+            this.Name = "FrmChangePassword";
+            this.Text = "Change Password";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnChange;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtCurrentPassword;
+        private System.Windows.Forms.TextBox TxtNewPassword;
+        private System.Windows.Forms.TextBox TxtReTypePassword;
+    }
+}
---
+++ StockControllerV20/StockControllerV20/FrmChangePassword.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockControllerV20
+{
+    public partial class FrmChangePassword : Form
+    {
+        public FrmChangePassword()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnChange_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
